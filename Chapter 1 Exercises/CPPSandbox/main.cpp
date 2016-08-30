2cf39747de28fe17c659620d0fbd28108f4171ae
Andy Meyers <andy.dude@gmail.com>
2016-08-30 08:39:30 -0500

    Initial Checkin and Chapter 1 exercises

---
+++ Chapter 1 Exercises/CPPSandbox/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  CPPSandbox
+//
+//  Created by Andrew Meyers on 8/30/16.
+//  Copyright © 2016 Andrew Meyers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+
+    std::cout << "Hello, World!\n";
+    std::cout << "This (\") is a quote, and this (\\) is a backslash.  This (\t) is a tab." << std::endl;
+
+    //create hello world cpp
+    
+    std::cout << "#include <iostream>" << std::endl;
+    std::cout << std::endl;
+    std::cout << "int main(int argc, const char * argv[]) { " << std::endl;
+    std::cout << std::endl;
+    std::cout << "    std::cout << \"Hello, world!\" << std::endl; " << std::endl;
+    std::cout << std::endl;
+    std::cout << "    return 0; " << std::endl;
+    std::cout << "}" << std::endl;
+    
+    
+    // insert code here...
+    return 0;
+}
